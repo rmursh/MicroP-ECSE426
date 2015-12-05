1a5c834e6392d44335bb64086ee195f4bb17ac68
unknown <rmursh@156TR4160MP-22.campus.MCGILL.CA>
2015-12-05 07:52:39 -0500

    Send data

--- LCD Base Project/src/main.c
+++ LCD Base Project/src/main.c
@@ -23,7 +23,8 @@
 
 #define RX_PKT 0x01
 
-uint8_t read[10];
+float read[30];
+float a;
 uint8_t read2[5][5];
 uint8_t plot[2];
 uint8_t status;
@@ -103,22 +104,23 @@ void setGrid(){
 
 void DisplayLCD(void const *argument){
 	char* s;
-	int cnt = 0;
+	int cnt, i = 0;
 	   LCD_Clear(LCD_COLOR_WHITE);
 	  //sprintf( s , "%d", read[0] ); 
 	  //The files source and header files implement drawing characters (drawing strings)
 	  //using different font sizes, see the file font.h for the four sizes
      setGrid();
-	while(1){
+	while(i < 30){
 		/* Clear the LCD */  		
 		//LCD_DrawCircle((uint16_t)(20+read[0]*0.862745), (uint16_t)(20+read[1]*1.176), 4 );
 		if(cnt > 1)
 		{
-			LCD_DrawUniLine((uint16_t)(120+read[0]*0.862745), (uint16_t)(160+read[1]*1.176), plot[0], plot[1]);
+			LCD_DrawUniLine((uint16_t)(120+read[i]*0.862745), (uint16_t)(160+read[i+1]*1.176), plot[0], plot[1]);
 		}
-		plot[0] = (uint16_t)(120+read[0]*0.862745);
-		plot[1] = (uint16_t)(160+read[1]*1.176);
+		plot[0] = (uint16_t)(120+read[i]*0.862745);
+		plot[1] = (uint16_t)(160+read[i+1]*1.176);
 		cnt++;
+		i = i + 2;
 
 		
 //	  LCD_SetTextColor(LCD_COLOR_BLUE2); 
@@ -158,10 +160,10 @@ void RxPacket(void const *argument){
    // put reciever in RX mode
 	CC2500_Strobe(CC2500_STROBE_SRX, 0x00);
 	
-   
+  //osSignalWait(RX_PKT, osWaitForever); 
 	while(1){
-		int i;
-		//osSignalWait(RX_PKT, osWaitForever);
+		int i = 0;
+		
 		status = CC2500_Strobe(CC2500_STROBE_SRX, 0x00);
 		CC2500_Read(&temp, CC2500_STATUS_REG_RXBYTES , 2);
 		CC2500_Read(&test, CC2500_STATUS_REG_PKTSTATUS , 2);
@@ -169,10 +171,15 @@ void RxPacket(void const *argument){
 		if(temp == CC2500_SETTING_PKTLEN)
 		{
 	  	status = CC2500_Strobe(CC2500_STROBE_SRX, 0x00);
-		  CC2500_RxPackets((uint8_t*)read2, CC2500_SETTING_PKTLEN );
+		  CC2500_RxPackets((uint8_t*)read, CC2500_SETTING_PKTLEN );
+//			i++;
+//			if(i >= 30)
+//			{
+//				i =0;
+//			}
 		}
 		osDelay(100);
-		printf("Read is %d %d \n", read[0], read[1]);
+		//printf("Read is %d %d \n", read[0], read[1]);
 	}
 }
 
